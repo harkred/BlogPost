75a2565c122e01da43e8371415b62bba8ea460ef
Harkeerat Singh <harkeerat2003@gmail.com>
2021-01-06 16:12:48 +0000

    update alpha:0.1.02-test

--- browse.py
+++ browse.py
@@ -4,7 +4,11 @@ from home import home
 def blog():
 	for child in main_frame.winfo_children():
 		child.pack_forget()
-	 
+	add_blog.config(state=DISABLED)
+	BLOG = Label(main_frame, text='Blog')
+	BLOG.pack(side='top')
+	
+		 
 
 def brew():
 	browse = Tk()
@@ -14,8 +18,13 @@ def brew():
 	sidebar_frame = Frame(browse)
 	sidebar_frame.pack(fill=Y, expand=1, side='left', anchor=W)
 
-	Button(sidebar_frame, text='Add Blog', command=blog).pack(anchor=NW)
-	Button(sidebar_frame, text='Log out', command=home).pack(anchor=W)
+	global add_blog
+	add_blog = Button(sidebar_frame, text='Add Blog', command=blog)
+	global log_out
+	log_out = Button(sidebar_frame, text='Log out', command=home)
+
+	add_blog.pack(anchor=NW)
+	log_out.pack(anchor=W)
 
 	global main_frame
 	main_frame = Frame(browse)
