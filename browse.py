1af95d8deda0c29e38f19fd3b300ec1a7adbbff8
TR0UBLESH00TER <74814583+TR0UBLESH00TER@users.noreply.github.com>
2021-01-06 22:39:12 +0530

    Edited Logout
    Edited Logout for browse.
    Now logout will destroy the window after log out. And show the home (login/register) window.

--- browse.py
+++ browse.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from home import home
 
+browse = ""    # Defining global variable 
+
 def blog():
 	for child in main_frame.winfo_children():
 		child.pack_forget()
@@ -19,7 +21,14 @@ def blog():
 	BLOG_CONT = Label(main_frame, text='Blog content: ')
 	BLOG_CONT.pack(side='left', anchor=W, pady=5) 
 
+def logout():
+	global browse
+	browse.destroy()
+	home()
+
 def brew():
+	global browse, add_blog, main_frame
+
 	browse = Tk()
 	browse.geometry("1100x700")
 
@@ -27,15 +36,13 @@ def brew():
 	sidebar_frame = Frame(browse)
 	sidebar_frame.pack(fill=Y, expand=1, side='left', anchor=W)
 
-	global add_blog
 	add_blog = Button(sidebar_frame, text='Add Blog', command=blog)
-	global log_out
-	log_out = Button(sidebar_frame, text='Log out', command=home)
+
+	log_out = Button(sidebar_frame, text='Log out', command=logout)
 
 	add_blog.pack(anchor=NW)
 	log_out.pack(anchor=W)
 
-	global main_frame
 	main_frame = Frame(browse)
 	main_frame.pack(fill=BOTH, expand=1, side='right', anchor=E)
 
