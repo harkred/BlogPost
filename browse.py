f3b753f7d84f85b4ac0e6351d0615122e6029073
Harkeerat Singh <harkeerat2003@gmail.com>
2021-01-06 15:41:18 +0000

    Update alpha:0.1

--- browse.py
+++ browse.py
@@ -1,11 +1,10 @@
 from tkinter import *
 
-def brow():
-	browse = Tk()
-	browse.geometry("1100x700")
+browse = Tk()
+browse.geometry("1100x700")
 
-	#Frames
-	sidebar_frame = Frame(browse)
-	sidebar_frame.pack(fill=Y, expand=1, side='left')
+#Frames
+sidebar_frame = Frame(browse)
+sidebar_frame.pack(fill=Y, expand=1, side='left')
 
-	mainloop()
+mainloop()
