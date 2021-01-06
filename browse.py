44790f1c4caa2210e22c7139a9f8b067ded7742b
Harkeerat Singh <harkeerat2003@gmail.com>
2021-01-06 15:48:55 +0000

    update alpha:0.1

--- browse.py
+++ browse.py
@@ -5,9 +5,12 @@ browse.geometry("1100x700")
 
 #Frames
 sidebar_frame = Frame(browse)
-sidebar_frame.pack(fill=Y, expand=1, side='left')
+sidebar_frame.pack(fill=Y, expand=1, side='left', anchor=W)
 
 Button(sidebar_frame, text='Add Blog').pack(anchor=NW)
 Button(sidebar_frame, text='Log out').pack(anchor=W)
+
+main_frame = Frame(browse)
+main_frame.pack(fill=BOTH, expand=1, side='right', anchor=E)
 
 mainloop()
