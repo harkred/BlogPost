96b11d933ad806f82de3b73710224d06110d41d9
TR0UBLESH00TER <74814583+TR0UBLESH00TER@users.noreply.github.com>
2021-01-25 19:51:22 +0530

    Added Beauty to Login
    :star: Switched to ttk widgets for login. Now it looks better :smile:

--- home.py
+++ home.py
@@ -1,8 +1,11 @@
 from tkinter import *
-import re
 from tkinter import messagebox
-from connections import write_register,result,login_results
+from tkinter import ttk
+
 import sqlite3
+import re
+
+from connections import write_register,result,login_results
 
 # Function to check password
 def check_password(event):
@@ -67,32 +70,32 @@ def home():
     root.resizable(0,0)
 
     # Frame for Login
-    login_frame = LabelFrame(root,text=" Login ",font=('Consolas', 10), labelanchor=N,width=200,height=700)
+    login_frame = LabelFrame(root,text=" Login ", labelanchor=N,width=200,height=700)
     login_frame.grid(row=0,column=0,padx=30,pady=20,sticky=N)
 
     # Username Label and Entry
-    Label(login_frame,text="Username: ",font=('Consolas', 10),padx=10,pady=5).grid(row=0,column=0)
-    Username = Entry(login_frame,width=40,font=('Consolas', 10), borderwidth=4)
+    Label(login_frame,text="Username: ",padx=10,pady=5).grid(row=0,column=0)
+    Username = ttk.Entry(login_frame,width=40)
     Username.grid(row=0,column=1,padx=5,pady=2)
     Username.bind('<Return>', lambda e: Password.focus_set())
 
     # Password Label and Entry
-    Label(login_frame,text="Password: ",font=('Consolas', 10),padx=10,pady=5).grid(row=1,column=0)
-    Password = Entry(login_frame,width=40,font=('Consolas', 10), borderwidth=4, show="•")
+    Label(login_frame,text="Password: ",padx=10,pady=5).grid(row=1,column=0)
+    Password = ttk.Entry(login_frame,width=40, show="•")
     Password.grid(row=1,column=1,padx=5,pady=2, sticky = W)
     Password.bind('<Return>', check_password)
 
     # Show Password? Checkbox
     Checked = IntVar()
-    Show_Password = Checkbutton(login_frame,text="Show Password",font=("Consolas",10),variable=Checked,command=show)
+    Show_Password = ttk.Checkbutton(login_frame,text="Show Password", variable=Checked, command=show)
     Show_Password.grid(row=2,column=1)
 
     # Button to Login and run check_password function
-    Login_Button=Button(login_frame,text="Login",width=20,font=("Consolas",10))
+    Login_Button = ttk.Button(login_frame,text="Login",width=20)
     Login_Button.grid(row=3,column=1,padx=5,pady=10)
     Login_Button.bind('<Button-1>', check_password)
 
-    Label(root,text="Not a member? Signup now! ",font=('Consolas', 10),padx=10,pady=5).grid(row=1,column=0,sticky=N)
+    Label(root,text="Not a member? Signup now! ",padx=10,pady=5).grid(row=1,column=0,sticky=N)
 
 # ----------------------------------------------------------------------------------------------- #
     # Frame for Register
@@ -101,33 +104,33 @@ def home():
 
 
     # Email Label and Entry
-    Label(register_frame,text="Email:        ",font=('Consolas', 10),padx=10,pady=5).grid(row=0,column=0)
-    Email = Entry(register_frame,width=40,font=('Consolas', 10), borderwidth=4)
+    Label(register_frame,text="Email:        ",padx=10,pady=5).grid(row=0,column=0)
+    Email = ttk.Entry(register_frame,width=40)
     Email.grid(row=0,column=1,padx=5,pady=2)
     Email.bind('<Return>', lambda e: RUsername.focus_set())
 
     # Username Label and Entry
-    Label(register_frame,text="Username:     ",font=('Consolas', 10),padx=10,pady=5).grid(row=1,column=0)
-    RUsername = Entry(register_frame,width=40,font=('Consolas', 10), borderwidth=4)
+    Label(register_frame,text="Username:     ",padx=10,pady=5).grid(row=1,column=0)
+    RUsername = ttk.Entry(register_frame,width=40)
     RUsername.grid(row=1,column=1,padx=5,pady=2)
     RUsername.bind('<Return>', lambda e: RPassword.focus_set())
 
     # Password Label and Entry
-    Label(register_frame,text="Password:     ",font=('Consolas', 10),padx=10,pady=5).grid(row=2,column=0)
-    RPassword = Entry(register_frame,width=40,font=('Consolas', 10), borderwidth=4)
+    Label(register_frame,text="Password:     ",padx=10,pady=5).grid(row=2,column=0)
+    RPassword = ttk.Entry(register_frame,width=40)
     RPassword.grid(row=2,column=1,padx=5,pady=2)
     RPassword.config(show="•")
     RPassword.bind('<Return>', lambda e: CPassword.focus_set())
 
     # Confirm Password Label and Entry
-    Label(register_frame,text="Confirm Password: ",font=('Consolas', 10),padx=10,pady=5).grid(row=3,column=0)
-    CPassword = Entry(register_frame,width=40,font=('Consolas', 10), borderwidth=4)
+    Label(register_frame,text="Confirm Password: ",padx=10,pady=5).grid(row=3,column=0)
+    CPassword = ttk.Entry(register_frame,width=40)
     CPassword.grid(row=3,column=1,padx=5,pady=2)
     CPassword.config(show="•")
     CPassword.bind('<Return>', check_password)
 
     # Button to Register and run register function
-    Register_Button=Button(register_frame,text="Register",width=20,command=register,font=("Consolas",10))
+    Register_Button = ttk.Button(register_frame,text="Register",width=20,command=register)
     Register_Button.grid(row=4,column=1,padx=5,pady=10)
     Register_Button.bind('<Button-1>', check_password)
 
